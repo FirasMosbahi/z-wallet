a1a8db244fa1ead1007e68109352cd64463fbbc1
firas <firasmosbahi10@gmail.com>
2023-05-05 21:31:41 +0100

    erc20 and erc721 implemented--v2

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,55 +4,73 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721Enumerable, Ownable{
+contract NFT is ERC721Enumerable, Ownable {
   using Strings for uint256;
-  string _baseURI;
+  string private baseURI;
   string public baseExtension = "json";
   string public baseImage = "webp";
-  bool public paused = false;
-  event Minted(
-    uint256 id,
-    address indexed nftOwner,
-    uint256 cost,
-    string indexed tokenURI,
-    uint256 timestamp
-  );
-  struct MintedStruct {
-    uint256 id;
-    address nftOwner;
-    uint256 cost;
-    string imageURL;
-    uint256 timestamp;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI
+  ) ERC721(_name, _symbol) {
+    baseURI = _initBaseURI;
   }
-  MintedStruct[] public minted;
-  constructor(string memory _name,string memory _symbol,string memory _initBaseURI) ERC721(_name,_symbol) {
-    _baseURI = _initBaseURI;
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
   }
-  function setBaseURI(string memory _newBaseURI) public  onlyOwner {
-    _baseURI = _newBaseURI;
-  }
-  function mint(uint256 _cost) public {
+
+  function mint() public {
     uint256 supply = totalSupply();
-    _safeMint(msg.sender, supply+1);
-    minted.push(MintedStruct(supply+1,msg.sender,_cost,toImage(supply+1),block.timestamp));
-    emit Minted(supply+1,msg.sender,_cost,tokenURI(supply+1),block.timestamp);
+    _safeMint(msg.sender, supply);
+    //    minted.push(MintedStruct(supply+1,msg.sender,toImage(supply+1),block.timestamp));
+    //    emit Minted(supply+1,msg.sender,tokenURI(supply+1),block.timestamp);
   }
-  function tokenURI(uint256 tokenId) public view virtual override returns (string memory){
-    require(_exists(tokenId),"ERC721Metadata:No token with this id");
-    return bytes(_baseURI).length > 0
-    ? string(abi.encodePacked(_baseURI,tokenId,baseImage))
+
+  function transferNFT(
+    address from,
+    address to,
+    uint256 tokenId
+  ) public {
+    require(
+      _isApprovedOrOwner(msg.sender, tokenId),
+      "ERC721: transfer caller is not owner nor approved"
+    );
+    _transfer(from, to, tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+  public
+  view
+  virtual
+  override
+  returns (string memory)
+  {
+    require(_exists(tokenId), "ERC721Metadata:No token with this id");
+    return
+    bytes(baseURI).length > 0
+    ? string(abi.encodePacked(baseURI, tokenId, baseImage))
     : "";
   }
-  function toImage(uint256 tokenId) internal view virtual returns (string memory) {
-    return bytes(_baseURI).length > 0
-    ? string(abi.encodePacked(_baseURI,tokenId,baseImage))
+
+  function toImage(uint256 tokenId)
+  public
+  view
+  virtual
+  returns (string memory)
+  {
+    return
+    bytes(baseURI).length > 0
+    ? string(abi.encodePacked(baseURI, tokenId, baseImage))
     : "";
   }
-  function getAnNFt(uint256 tokenId) public view returns (MintedStruct memory){
-    return minted[tokenId-1];
-  }
-  function transferTo(address to, uint256 tokenId) public {
-    require(msg.sender == minted[tokenId-1].nftOwner , "only the owner of the nft can send it");
-    minted[tokenId-1].nftOwner = to;
-  }
+  //  function getAnNFt(uint256 tokenId) public view returns (MintedStruct memory){
+  //    return minted[tokenId-1];
+  //  }
+  //  function transferTo(address to, uint256 tokenId) public {
+  //    require(msg.sender == minted[tokenId-1].nftOwner , "only the owner of the nft can send it");
+  //    minted[tokenId-1].nftOwner = to;
+  //  }
 }
--- contracts/ZCoin.sol
+++ contracts/ZCoin.sol
@@ -4,41 +4,77 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract ZCoin is ERC20Capped , ERC20Burnable{
+contract ZCoin is ERC20Capped, ERC20Burnable {
   address payable public owner;
   uint256 public blockReward;
-  uint256 public initialValue;
   uint256 public cappedAt;
-  ufixed8x8 public rate;
-  modifier onlyOwner {
-    require(msg.sender == owner,"Only the owner can call this function");
+  uint256 public rate;
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only the owner can call this function");
     _;
   }
-  constructor(uint256 _blockReward,uint256 initialValue,uint256 _cappedAt) ERC20("Z-Token" , "ZTK") ERC20Capped(cappedAt * (10**18)) {
+
+  constructor(
+    uint256 _blockReward,
+    uint256 _initialValue,
+    uint256 _cappedAt,
+    uint256 _rate
+  ) ERC20("Z-Token", "ZTK") ERC20Capped(cappedAt * (10**18)) {
     owner = payable(msg.sender);
-    miningReward = _miningReward * (10**18);
     cappedAt = _cappedAt * (10**18);
-    _mint(owner,initialValue * (10**18));
+    _mint(owner, _initialValue * (10**18));
     blockReward = _blockReward * (10**18);
-  }
-  function _mint(address account,uint256 amount) internal virtual override(ERC20Capped, ERC20) {
-    require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
-    super._mint(account, amount);
-  }
-  function _mintMinerReward() internal {
-    _mint(block.coinbase,blockReward);
-  }
-  function _beforeTokenTransfer(address from,address to,uint256 value) internal virtual override{
-    if(from != address(0) && to != block.coinbase && block.coinbase != address(0)){
-      _mintMinerReward();
-    }
-    super._beforeTokenTransfer(from,to,value);
-  }
-  function setBlockReward(uint256 reward) public onlyOwner {
-    blockReward = reward * (10**18);
+    rate = _rate;
   }
 
-  function destroy() public onlyOwner {
-    selfdestruct(owner);
+  function _mint(address account, uint256 amount)
+  internal
+  virtual
+  override(ERC20Capped, ERC20)
+  {
+    require(
+      ERC20.totalSupply() + amount <= cap(),
+      "ERC20Capped: cap exceeded"
+    );
+    super._mint(account, amount);
   }
+
+  function _mintMinerReward() internal {
+    _mint(block.coinbase, blockReward);
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 value
+  ) internal virtual override {
+    if (
+      from != address(0) &&
+      to != block.coinbase &&
+      block.coinbase != address(0)
+    ) {
+      _mintMinerReward();
+    }
+    super._beforeTokenTransfer(from, to, value);
+  }
+
+  function buyWithEtherum() public payable {
+    _mint(msg.sender, msg.value * rate);
+  }
+
+  function transferTo(address to, uint256 amount) public {
+    _transfer(msg.sender, to, amount);
+    emit Transfer(msg.sender, to, amount);
+  }
+
+  function setBlockReward(uint256 _reward) public onlyOwner {
+    blockReward = _reward * (10**18);
+  }
+
+  function setRate(uint256 _rate) public onlyOwner {
+    rate = _rate;
+  }
+  //function destroy() public onlyOwner {
+  //  selfdestruct(owner);
+  //}
 }

f19e09803c5b6705ff87bc97d7a9433df9263ccf
firas <firasmosbahi10@gmail.com>
2023-05-10 20:02:02 +0100

    contracts-update

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+!// SPDX-License-Identifier: MIT
 //TODO: ad setters for nft attributes and add events
 pragma solidity ^0.8.0;
 
@@ -13,12 +13,14 @@ contract MyNFT is ERC721URIStorage {
 
     struct NFTData {
         uint256 id;
+        string name;
+        string description;
         string uri;
         uint256 cost;
         bool isForSale;
         address owner;
     }
-    event NFTMinted(uint256 id,string uri,uint256 cost,bool isForSale,address owner);
+    event NFTMinted(uint256 id,string name,string description,string uri,uint256 cost,bool isForSale,address owner);
     event NFTSold(uint256 id,address buyer,address seller,uint256 price);
     mapping(uint256 => NFTData) public allNFTs;
 
@@ -32,12 +34,13 @@ contract MyNFT is ERC721URIStorage {
     //        delete allNFTs[tokenId].uri;
     //    }
     //}
-
-    function createCollectible(string memory _tokenURI,uint256 _cost,bool _isForSale) public payable returns (uint256) {
+    function createCollectible(string memory _tokenURI,string memory _name,string memory _description,uint256 _cost,bool _isForSale) public payable returns (uint256) {
         require(msg.value == price_in_wei, "You need ether to mint NFT");
         NFTData memory newNFT;
         newNFT.id = tokenCounter;
         tokenCounter += 1;
+        newNFT.name = _name;
+        newNFT.description = _description;
         newNFT.uri = _tokenURI;
         newNFT.cost = _cost;
         newNFT.isForSale = _isForSale;
@@ -46,14 +49,16 @@ contract MyNFT is ERC721URIStorage {
         _mint(msg.sender, newNFT.id);
         _setTokenURI(newNFT.id, _tokenURI);
         payable(address(this)).transfer(msg.value);
-        emit NFTMinted(newNFT.id, newNFT.uri, newNFT.cost, newNFT.isForSale, newNFT.owner);
+        emit NFTMinted(newNFT.id, newNFT.name, newNFT.description, newNFT.uri, newNFT.cost, newNFT.isForSale, newNFT.owner);
         return newNFT.id;
     }
 
-    function mintWithToken(string memory _tokenURI,uint256 _cost,bool _isForSale) public returns (uint256) {
+    function mintWithToken(string memory _tokenURI,string memory _name,string memory _description,uint256 _cost,bool _isForSale) public returns (uint256) {
         require(token.balanceOf(msg.sender) >= price_ini_ztk, "You need ZToken to mint NFT");
         NFTData memory newNFT;
         newNFT.id = tokenCounter;
+        newNFT.name = _name;
+        newNFT.description = _description;
         tokenCounter += 1;
         newNFT.uri = _tokenURI;
         newNFT.cost = _cost;
@@ -77,5 +82,14 @@ contract MyNFT is ERC721URIStorage {
         _transfer(previousOwner, msg.sender, _tokenId);
         token.transferFrom(msg.sender, previousOwner, nft.cost);
         emit NFTSold(_tokenId, previousOwner, msg.sender, nft.cost);
+    }
+    function setNFTName(uint256 _id,string memory _name) public {
+        allNFTs[_id].name = _name;
+    }
+    function setNFTDescription(uint256 _id,string memory _description) public {
+        allNFTs[_id].description = _description;
+    }
+    function setNFTIsForSale(uint256 _id,bool _isForSale) public {
+        allNFTs[_id].isForSale = _isForSale;
     }
 }
